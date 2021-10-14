8eec6f214e76dcb06416f7ff1dbf0b01daf73a05
Samuel <71878361+SamBolognese@users.noreply.github.com>
2021-10-14 17:34:04 +0200

    Add project files.

---
+++ BlazorExcercise/Data/Catalogue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorExcercise.Data
+{
+    public class Catalogue
+    {
+        public delegate void ChangeHandler();
+        public event ChangeHandler CatalogueChanged;
+        private List<Product> products = new List<Product>();
+        public IEnumerable<Product> AllProducts()
+        {
+            return products;
+        }
+        public void AddProduct(Product p)
+        {
+            products.Add(p);
+            CatalogueChanged.Invoke();
+        }
+        public void RemoveProduct(Product p)
+        {
+            products.Remove(p);
+            CatalogueChanged.Invoke();
+        }
+        public void UpdateProduct(Product p)
+        {
+            CatalogueChanged.Invoke();
+        }
+    }
+}
---
+++ BlazorExcercise/Data/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorExcercise.Data
+{
+    public class Product
+    {
+        private string name;
+        private double price;
+        
+        [Required]
+        [StringLength(20, ErrorMessage = "The name cannot be longer than 20 characters")]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [Required]
+        [Range(0, 9999)]
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Price: {Price}";
+        }
+    }
+}
